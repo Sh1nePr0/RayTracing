2c8be5e19a9fb17071660c2a0d8ce6b675969cb4
Sh1nePr0 <fedoreev77@mail.ru>
2024-10-02 01:27:46 +0300

    Remove demo ImGui app and create random rendering image in realtime

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,24 +2,73 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
+
+using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("App Settings");
+		ImGui::Text("Last render time : %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Rendering"))
+		{
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("App Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if(m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
 	}
+
+	void Render()
+	{
+		Timer timer;
+
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageDataBuffer;
+			m_ImageDataBuffer = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+		{
+			m_ImageDataBuffer[i] = Random::UInt();
+			m_ImageDataBuffer[i] |= 0xff000000;
+		}
+
+		m_Image->SetData(m_ImageDataBuffer);
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	std::shared_ptr<Image> m_Image;
+	uint32_t m_ViewportWidth = 0;
+	uint32_t m_ViewportHeight = 0;
+	uint32_t* m_ImageDataBuffer = nullptr;
+	float m_LastRenderTime = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "Ray Tracing App";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
