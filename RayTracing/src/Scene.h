f7bb2f1c4d855f5ef8a0fe9b140be118177bb9c2
Sh1nePr0 <fedoreev77@mail.ru>
2024-12-10 15:40:17 +0300

    Create little material system and update renderer and main app for best looks reflection in spheres.

--- RayTracing/src/Scene.h
+++ RayTracing/src/Scene.h
@@ -4,16 +4,24 @@
 
 #include <glm/glm.hpp>
 
+struct Material
+{
+	//Not lit material color
+	glm::vec3 Albedo{ 1.0f };
+	float Roughness = 1.0f;
+	float Metallic = 0.0f;
+};
+
 struct Sphere
 {
 	glm::vec3 Position{0.0f};
 	float Radius = 0.5f;
 
-	//Not lit material color
-	glm::vec3 Albedo{1.0f};
+	int MaterialIndex = 0;
 };
 
 struct Scene
 {
 	std::vector<Sphere> Spheres;
+	std::vector<Material> Materials;
 };
